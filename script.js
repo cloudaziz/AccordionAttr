7af04ff252fc4983bdb3429b45b62171524d27c2
Mohammed Abdul Aziz <cloudaziz@gmail.com>
2023-06-18 13:20:53 +0600

    adding js code into the script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('active');
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove('active');
+    });
+    let target = document.querySelector(tab.dataset.tabTarget);
+    target.classList.add('active');
+
+    tab.classList.add('active');
+  });
+});
